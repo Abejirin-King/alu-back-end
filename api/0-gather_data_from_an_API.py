0190aecec37161c02598f22eaab945a09ed9e763
King Abejrin <k.abejirin@alustudent.com>
2025-07-13 06:00:16 -0700

    zero

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,39 +1,28 @@
 #!/usr/bin/python3
-"""
-Fetch and display the TODO list progress of an employee from a REST API.
-"""
+""" Import libraries """
 
 import requests
 import sys
 
-
-def fetch_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        return
-
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
-
-    todos_response = requests.get(f"{base_url}/todos", params={"userId": employee_id})
-    todos = todos_response.json()
-
-    total_tasks = len(todos)
-    done_tasks = [task for task in todos if task.get("completed")]
-
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, len(done_tasks), total_tasks))
-
-    for task in done_tasks:
-        print("\t {}".format(task.get("title")))
-
+"""Gathering data from an API """
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        sys.exit(1)
+    employee_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
 
-    emp_id = int(sys.argv[1])
-    fetch_employee_todo_progress(emp_id)
+    todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
+    todo = todo.format(employee_id)
 
+    user_info = requests.request("GET", url).json()
+
+    todo_info = requests.request("GET", todo).json()
+
+    employee_name = user_info.get("name")
+    total_tasks = list(filter(lambda x: (x["completed"] is True), todo_info))
+    task_com = len(total_tasks)
+    total_task_done = len(todo_info)
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+          task_com, total_task_done))
+
+    [print("\t {}".format(task.get("title"))) for task in total_tasks]
